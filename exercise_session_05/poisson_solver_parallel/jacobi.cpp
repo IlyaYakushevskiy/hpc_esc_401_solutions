182499780532e87271473854dd03101c47fe7178
Illia Yakushevskyi <iyakushe@eiger-ln004.cscs.ch>
2025-10-24 12:37:41 +0200

    task 2 done

---
+++ exercise_session_05/poisson_solver_parallel/jacobi.cpp
@@ -0,0 +1,55 @@
+#include "jacobi.h"
+#include <math.h>
+
+/**
+ * @brief      Computes norm of the difference between two matrices
+ *
+ * @param[in]  p     Parameters
+ * @param      mat1  The matrix 1
+ * @param      mat2  The matrix 2
+ *
+ * @return     Returns \sqrt{\sum(mat1_{ij} - mat2_{ij})^2/(nx*ny)}
+ */
+double norm_diff(params p, double** mat1, double** mat2){
+    double norm_sq_sum = 0.0; 
+
+    #pragma omp parallel for collapse(2) reduction(+:norm_sq_sum) 
+    for (int i = 1; i <= p.nx; i++){ 
+        for (int j = 1; j <= p.ny; j++){ 
+            norm_sq_sum += (mat1[i][j] - mat2[i][j]) * (mat1[i][j] - mat2[i][j]);
+        }
+    }
+    return sqrt(norm_sq_sum / (double)(p.nx * p.ny)); 
+}
+
+/**
+ * @brief      Compute a Jacobi iteration to solve Poisson equation. This function updates
+ *             u_new and u_old in-place.
+ *
+ * @param[in]  p      Parameters
+ * @param      u_new  The new solution
+ * @param      u_old  The old solution
+ * @param      f      The source term
+ */
+void jacobi_step(params p, double** u_new, double** u_old, double** f){
+    double dx = 1.0 / (p.nx + 1); 
+    double dy = 1.0 / (p.ny + 1); 
+
+   
+
+    #pragma omp parallel for collapse(2) 
+    for (int i = 0; i <= p.nx + 1; i++){
+        for (int j = 0; j <= p.ny + 1; j++){
+            u_old[i][j] = u_new[i][j]; 
+        }
+    }
+
+    #pragma omp parallel for collapse(2) 
+    for (int i = 1; i <= p.nx; i++){ 
+        for (int j = 1; j <= p.ny; j++){ 
+            u_new[i][j] = 0.25 * (u_old[i+1][j] + u_old[i-1][j] + 
+                                  u_old[i][j+1] + u_old[i][j-1] - 
+                                  dx*dy*f[i][j]);
+        }
+    }
+}
---
+++ exercise_session_05/poisson_solver_parallel/main.cpp
@@ -0,0 +1,48 @@
+#include "io.h"
+#include "init.h"
+#include "jacobi.h"
+#include <omp.h> 
+
+int main (int argc, char *argv[]){
+
+    const char* file_name="params.txt";
+
+    // Read the parameter file and store information in a params structure (defined in init.h)
+    params p;
+    readParameters(file_name, &p);
+
+    // Initialize the matrices used in the Jacobi iteration
+    double **f, **u_old, **u_new;
+
+    // First allocate memory for each matrix
+    f = allocateGrid(p.nx + 2, p.ny + 2, f);
+    u_old = allocateGrid(p.nx + 2, p.ny + 2, u_old);
+    u_new = allocateGrid(p.nx + 2, p.ny + 2, u_new);
+
+    // Initialize the value of matrices
+    init_variables(p, f, u_old, u_new);
+    printf("initialization done.\n");
+    // Output the source term of the Poisson equation in a csv file
+    output_source(p, f);
+
+    // Do a first jacobi step
+    jacobi_step(p, u_new, u_old, f);
+    printf("step 1 done.\n");
+
+    // Compute differences and norm
+    double diff = norm_diff(p, u_new, u_old);
+    printf("Initial diff=%g\n", diff);
+    // Initialize the Jacobi step conter
+    int nstep=1;
+
+    // Main loop for the Jacobi iterations
+    while (diff>p.tol && nstep<p.nstep_max){
+        jacobi_step(p, u_new, u_old, f);
+        diff = norm_diff(p, u_new, u_old);
+        nstep++;
+        printf("Step %d, Diff=%g\n", nstep, diff);
+        if (nstep%p.foutput==0)
+            output(p, nstep, u_new);
+    }
+    return 0;
+}
