9477c02848b495bd5964267a6764e0a3be5a4cd3
Mr Y <iyakushe@eiger-ln004.cscs.ch>
2025-12-05 12:49:25 +0100

    HW 11.3 done

--- exercise_session_11/README.md
+++ exercise_session_11/README.md
@@ -3,23 +3,26 @@
 ## Ex1
 Cool! Make generated 2 executables, the original and the one with summary from craypat. The summary is quite informative, it goes over CPU specs and then most importantly tells where in our code cpu did the most of work, namely : 
 
-```3||  82.7% | 1,045.0 | hpc_esc_401_solutions/exercise_session_11/nbody/nbody.cpp
+**```**3||  82.7% | 1,045.0 | hpc_esc_401_solutions/exercise_session_11/nbody/nbody.cpp
 ||||-------------------------------------------------------------------------
 4|||   2.8% |    36.0 | line.38
 4|||  23.0% |   291.0 | line.39
 4|||  56.8% |   718.0 | line.40
 ||||=========================================================================
 3||  17.3% |   219.0 | c++/12/bits/stl_vector.h
-4||        |         |  line.386```
+4||        |         |  line.386
+**```**
 
 where we see those are lines to calculate forces which take take of the most time 
+**```**
 	    ax += dx * ir3;
 	    ay += dy * ir3;
 	    az += dz * ir3;
+**```**        
 it makes absolute sense since it's calculating update with the main formula which has O(n^2) complexity, namely $ F_ij = (M_i * M_j)* e_i / |r^3| $  
 
 FULL OUTPUT : 
-```
+**```**
 CrayPat/X:  Version 23.12.0 Revision 67ffc52e7 sles15.4_x86_64  11/13/23 21:04:20
 
 #################################################################
@@ -147,4 +150,33 @@ To see the entire call tree:
 
 For interactive, graphical performance analysis, run:
   app2 /users/iyakushe/hpc_esc_401_solutions/exercise_session_11/nbody/nbody+119383-1773762609s
-  ```
+**```**
+
+## Ex2 
+
+from the last run we've seen Avg Process Time: "12.72 secs". Now we have something close but a bit worse. Now working on the sqrt() problem. 
+
+**```**
+iyakushe@eiger-ln004:~/hpc_esc_401_solutions/exercise_session_11/nbody> time ./nbody
+
+real    0m12.828s
+user    0m12.823s
+sys     0m0.002s
+**```**
+
+Now the code works only with float level of precision. We cut the time almost in half, impressive! 
+
+**```**
+real    0m7.330s
+user    0m7.245s
+sys     0m0.000s
+**```**
+
+## Ex3 
+
+Im vectorized the code and ,actually , it performs much worse now , I suspect a divergence which is a part of a next problem ... 
+real    0m16.554s
+user    0m16.550s
+sys     0m0.001s
+
+## Ex4 
--- exercise_session_11/nbody/nbody.cpp
+++ exercise_session_11/nbody/nbody.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 using std::vector;
 
-struct particle {
-    float x, y, z;	// position
-    float vx, vy, vz;	// velocity
-    float ax, ay, az;	// acceleration
+struct particles {
+    vector<float> x, y, z;	// position
+    vector<float> vx, vy, vz;	// velocity
+    vector<float> ax, ay, az;	// acceleration
 };
-typedef vector<particle> particles;
+
 
 // Initial conditions
 void ic(particles &plist, int n) {
@@ -15,36 +15,58 @@ void ic(particles &plist, int n) {
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_real_distribution<float> dis(0.0, 1.0);
 
-    plist.clear(); 	// Remove all particles
-    plist.reserve(n);	// Make room for "n" particle
-    for( auto i=0; i<n; ++i) {
-	particle p { dis(gen),dis(gen),dis(gen),0,0,0,0,0,0 };
-	plist.push_back(p);
+    plist.x.resize(n);
+    plist.y.resize(n);
+    plist.z.resize(n);
+
+    plist.vx.resize(n, 0.0f);
+    plist.vy.resize(n, 0.0f);
+    plist.vz.resize(n, 0.0f);
+    plist.ax.resize(n, 0.0f);
+    plist.ay.resize(n, 0.0f);
+    plist.az.resize(n, 0.0f);
+    
+	for( int i = 0; i < n; ++i) {
+        plist.x[i] = dis(gen);
+        plist.y[i] = dis(gen);
+        plist.z[i] = dis(gen);
     }
 
 }
 
 void forces(particles &plist) {
-    int n = plist.size();
-    for(int i=0; i<n; ++i) { 		// We want to calculate the force on all particles
-	float ax=0, ay=0, az=0;
-        for(int j=0; j<n; ++j) {			// Depends on all other particles
-	    if (i==j) continue; // Skip self interaction 
-	    auto dx = plist[j].x - plist[i].x;
-	    auto dy = plist[j].y - plist[i].y;
-	    auto dz = plist[j].z - plist[i].z;
-	    auto r = sqrt(dx*dx + dy*dy + dz*dz);
-	    auto ir3 = 1 / (r*r*r);
-	    ax += dx * ir3;
-	    ay += dy * ir3;
-	    az += dz * ir3;
-	}
-	plist[i].ax = ax;
-	plist[i].ay = ay;
-	plist[i].az = az;
+    int n = plist.x.size();
+    
+    for(int i = 0; i < n; ++i) {
+        plist.ax[i] = 0.0f;
+        plist.ay[i] = 0.0f;
+        plist.az[i] = 0.0f;
+    }
+
+  
+    for(int i = 0; i < n; ++i) {       
+        for(int j = 0; j < n; ++j) {    
+            if (i == j) continue; 
+            
+           
+            auto dx = plist.x[j] - plist.x[i];
+            auto dy = plist.y[j] - plist.y[i];
+            auto dz = plist.z[j] - plist.z[i];
+            
+       
+            auto r_sq = dx*dx + dy*dy + dz*dz;
+            auto r = sqrtf(r_sq);
+            
+          
+            auto ir3 = 1.0f / (r * r_sq); 
+            
+            
+            plist.ax[i] += dx * ir3;
+            plist.ay[i] += dy * ir3;
+            plist.az[i] += dz * ir3;
+        }
     }
 }
-
 int main(int argc, char *argv[]) {
     int N=50'000;
     particles plist;
